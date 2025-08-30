88562cbb258e0f9a2ebdca120dea9e90f6a324ba
Maxim Samsonov <m.samsonov@computer.org>
2025-08-30 13:53:21 +0300

    s3

---
+++ MediaPi.Core/RestModels/DeviceRegisterRequest.cs
@@ -0,0 +1,10 @@
+﻿// Developed by Maxim [maxirmx] Samsonov (www.sw.consulting)
+// This file is a part of Media Pi backend application
+
+namespace MediaPi.Core.RestModels;
+
+public sealed class DeviceRegisterRequest
+{
+    public string PublicKeyOpenSsh { get; set; } = "";
+    public string? SshUser { get; set; }
+}
