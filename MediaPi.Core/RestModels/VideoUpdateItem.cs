de68d05f2cfacc2f6b341305496716a8beaab2ca
Maxim Samsonov <m.samsonov@computer.org>
2025-10-10 01:24:08 +0300

    clean 2

--- MediaPi.Core/RestModels/VideoUpdateItem.cs
+++ MediaPi.Core/RestModels/VideoUpdateItem.cs
@@ -1,15 +1,11 @@
 // Copyright (c) 2025 sw.consulting
 // This file is a part of Media Pi backend
 
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-
 namespace MediaPi.Core.RestModels;
 
 public class VideoUpdateItem
 {
-    [Required]
-    public string Title { get; set; } = string.Empty;
+    public required string Title { get; set; } = string.Empty;
 
     public List<int>? PlaylistIds { get; set; }
 }
