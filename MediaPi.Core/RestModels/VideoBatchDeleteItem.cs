e1780b580fb1c4c3fed8d8358db4c75c1f23ceab
Maxim Samsonov <maxirmx@sw.consulting>
2026-05-25 17:45:04 +0300

    feat: allow video batch delete (#94)

---
+++ MediaPi.Core/RestModels/VideoBatchDeleteItem.cs
@@ -0,0 +1,23 @@
+// Copyright (C) 2025-2026 sw.consulting
+// This file is a part of Media Pi backend
+
+namespace MediaPi.Core.RestModels;
+
+public class VideoBatchDeleteItem
+{
+    public List<int> Ids { get; set; } = [];
+}
+
+public class VideoBatchDeleteResult
+{
+    public int RequestedCount { get; init; }
+    public List<int> DeletedIds { get; init; } = [];
+    public List<VideoBatchDeleteFailure> Failures { get; init; } = [];
+}
+
+public class VideoBatchDeleteFailure
+{
+    public required int Id { get; init; }
+    public required string Reason { get; init; }
+    public required string Message { get; init; }
+}
