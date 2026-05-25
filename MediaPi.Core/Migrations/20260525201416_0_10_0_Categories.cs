ac7227b4af986c96a4d1ea8345cda473cc3fc506
Maxim Samsonov <m.samsonov@computer.org>
2026-05-25 23:14:42 +0300

    update category model

---
+++ MediaPi.Core/Migrations/20260525201416_0_10_0_Categories.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MediaPi.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class _0_10_0_Categories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "free",
+                table: "categories",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "free",
+                table: "categories");
+        }
+    }
+}
